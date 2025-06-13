72ac6a3110071432213daf7cd38563ee41c89a22
bolakecil <jeslynn30@gmail.com>
2025-06-13 21:31:15 +0700

    Added PJS font, finished UI for start page

--- Lumea/Utilities/Font+Custom.swift
+++ Lumea/Utilities/Font+Custom.swift
@@ -4,4 +4,9 @@ extension Font {
     static func bethany(size: CGFloat) -> Font {
         .custom("Bethany Elingston", size: size)
     }
+    
+    static func jakarta(size: CGFloat) -> Font {
+        .custom("PlusJakartaSans-VariableFont_wght", size: size)
+    }
 }
+
--- Lumea/View/StartView.swift
+++ Lumea/View/StartView.swift
@@ -2,18 +2,35 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        VStack{
-            Image("shade")
-                .resizable()
-                .rotationEffect(.degrees(90))
-                .frame(width: 600, height: 400)
-            Text("Find your perfect Makeup Shade")
-                .font(.bethany(size: 30))
+        ZStack {
+            Color.background
+                .ignoresSafeArea(edges: .all)
+            VStack{
+                Image("shade")
+                    .resizable()
+                    .rotationEffect(.degrees(90))
+                    .frame(width: 600, height: 400)
+                Text("Find your perfect Makeup Shade")
+                    .font(.bethany(size: 35))
+                    .padding(.top, 40)
+                Text("Get personalized shade recommendations that blend seamlessly with your unique skin tone and undertone")
+                    .font(.jakarta(size: 17))
+                    .padding(.top, 10)
+                    .padding(.horizontal, 125)
+                    .multilineTextAlignment(.center)
+                Button(action: {
+                    print("Button tapped")
+                }) {
+                    Text("Get Started")
+                        .font(.jakarta(size: 18))
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 70)
+                        .background(Color.secondary)
+                        .cornerRadius(20)
+                }
                 .padding(.top, 50)
-            Text("Get personalized shade recommendations that blend seamlessly with your unique skin tone and undertone")
-                .font(.bethany(size: 23))
-                .padding(.top, 20)
-                .multilineTextAlignment(.center)
+            }
+            .padding(.horizontal, 50)
         }
     }
 }
