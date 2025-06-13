da7e39c72c539c823f821adbb4de3f94fdf1c9ba
bolakecil <jeslynn30@gmail.com>
2025-06-13 15:38:45 +0700

    Initial

---
+++ Lumea/LumeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumeaApp.swift
+//  Lumea
+//
+//  Created by Jessica Lynn on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LumeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
